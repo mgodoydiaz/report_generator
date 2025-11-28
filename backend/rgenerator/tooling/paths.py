f2d60a4918920771b5697f6ee315df1bce719e6f
Miguel Godoy <magodoydiaz@gmail.com>
2025-11-28 17:33:59 -0300

    Intermedio para trabajar en otro pc

--- backend/rgenerator/tooling/paths.py
+++ backend/rgenerator/tooling/paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import tomllib
 
-BASE_DIR = Path(__file__).resolve().parents[2] # Sube desde backend/rgenerator hasta la raíz del proyecto
+BASE_DIR = Path(__file__).resolve().parents[3] # Sube desde backend/rgenerator hasta la raíz del proyecto
 CONFIG_PATH = BASE_DIR / "config" / "settings.toml"
 
 config = tomllib.loads(CONFIG_PATH.read_text(encoding="utf-8"))
@@ -9,3 +9,4 @@ config = tomllib.loads(CONFIG_PATH.read_text(encoding="utf-8"))
 INPUT_DIR = BASE_DIR / config["paths"]["input_dir"]
 OUTPUT_DIR = BASE_DIR / config["paths"]["output_dir"]
 TMP_DIR = BASE_DIR / config["paths"]["tmp_dir"]
+SCHEMAS_DIR = BASE_DIR / config["reports"]["schemas_dir"]
