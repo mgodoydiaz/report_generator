3fc8046b227f0b5ae9d788323d094a9a691ae03a
Miguel Godoy <magodoydiaz@gmail.com>
2026-01-21 13:46:25 -0300

    Version estable de frontend

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,59 +5,36 @@ const linkClass = ({ isActive }) =>
 
 export default function Sidebar() {
   return (
-    <div
-      className="offcanvas-lg offcanvas-start"
-      tabIndex="-1"
-      id="mainMenu"
-      aria-labelledby="mainMenuLabel"
-    >
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="mainMenuLabel">
-          Menu
-        </h5>
-        <button
-          type="button"
-          className="btn-close"
-          data-bs-dismiss="offcanvas"
-          aria-label="Cerrar"
-        ></button>
+    <aside className="sidebar" aria-label="Menu principal">
+      <div className="sidebar-header">
+        <h5 className="sidebar-title">Menu</h5>
       </div>
-      <div className="offcanvas-body">
-        <div className="nav flex-column nav-pills gap-2">
-          <NavLink className={linkClass} to="/" data-bs-dismiss="offcanvas">
-            Inicio
-          </NavLink>
-          <NavLink
-            className={linkClass}
-            to="/evaluaciones"
-            data-bs-dismiss="offcanvas"
-          >
-            Evaluaciones
-          </NavLink>
-          <a className="nav-link" href="#" data-bs-dismiss="offcanvas">
-            Plantillas
-          </a>
-          <NavLink
-            className={linkClass}
-            to="/resultados"
-            data-bs-dismiss="offcanvas"
-          >
-            Resultados
-          </NavLink>
-          <a className="nav-link" href="#" data-bs-dismiss="offcanvas">
-            Valores
-          </a>
-          <a className="nav-link" href="#" data-bs-dismiss="offcanvas">
-            Analisis y alertas
-          </a>
-          <a className="nav-link" href="#" data-bs-dismiss="offcanvas">
-            Ayuda
-          </a>
-          <a className="nav-link" href="#" data-bs-dismiss="offcanvas">
-            Configuracion
-          </a>
-        </div>
+      <div className="nav flex-column nav-pills gap-2">
+        <NavLink className={linkClass} to="/">
+          Inicio
+        </NavLink>
+        <NavLink className={linkClass} to="/evaluaciones">
+          Evaluaciones
+        </NavLink>
+        <a className="nav-link" href="#">
+          Plantillas
+        </a>
+        <NavLink className={linkClass} to="/resultados">
+          Resultados
+        </NavLink>
+        <a className="nav-link" href="#">
+          Valores
+        </a>
+        <a className="nav-link" href="#">
+          Analisis y alertas
+        </a>
+        <a className="nav-link" href="#">
+          Ayuda
+        </a>
+        <a className="nav-link" href="#">
+          Configuracion
+        </a>
       </div>
-    </div>
+    </aside>
   );
 }
--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -3,16 +3,6 @@
     <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
       <div className="container-fluid">
         <span className="navbar-brand fw-bold">Menu</span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#mainMenu"
-          aria-controls="mainMenu"
-          aria-label="Abrir menu"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
       </div>
     </nav>
   );
